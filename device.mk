6a94fa97ea68f0664e52d15a8a71054ccfbc6f66
lollipuru <banstellazz@gmail.com>
2025-01-22 10:32:44 +0000

    vermeer: build sensors.xiaomi.v2
    Change-Id: I44316260aa3142525ec6b412147a33d06cb14809
    Signed-off-by: lollipuru <banstellazz@gmail.com>

--- device.mk
+++ device.mk
@@ -26,6 +26,13 @@ PRODUCT_PACKAGES += \
     WifiResVermeerPOCO \
     WifiResVermeerRedmi
 
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.xiaomi.v2
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/hals.conf:$(TARGET_COPY_OUT_ODM)/etc/sensors/hals.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
