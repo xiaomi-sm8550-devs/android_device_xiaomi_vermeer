c2bb53ad8f086b79c0272d1eb63fc4e6237c9c51
ByteWave1014 <jvillaltamatos3007@gmail.com>
2025-02-18 11:15:14 -0500

    vermeer: Commonize device especific kernel modules Signed-off-by: Lunark :3 <jvillaltamatos3007@gmail.com>
    Change-Id: I960c1e668ee395eceec1eca831ccac06f8610bd4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,3 +11,12 @@ include device/xiaomi/sm8550-common/BoardConfigCommon.mk
 include vendor/xiaomi/vermeer/BoardConfigVendor.mk
 
 DEVICE_PATH := device/xiaomi/vermeer
+
+# Kernel
+BOARD_VENDOR_KERNEL_MODULES_LOAD += \
+	aw882xx_dlkm.ko \
+	goodix_fod.ko \
+	goodix_core.ko \
+        wl2868c.ko \
+        wl2866d.ko \
+        xiaomi_touch.ko
