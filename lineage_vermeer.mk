5a1dbb523643d1880be4063483edc322d05f1c82
Lunark :3 <jvillaltamatos3007@gmail.com>
2026-01-10 11:05:00 -0500

    vermeer: Update from OS3.0.6.0.WNKMIXM
    - Also update some camera sensor for Redmi K70 to OS3.0.6.0.WNKCNXM
    
    Change-Id: I2dc397cdabb80bfa1b2fed4b3fb2fb1db2711dbf
    Signed-off-by: Lunark :3 <jvillaltamatos3007@gmail.com>

--- lineage_vermeer.mk
+++ lineage_vermeer.mk
@@ -23,4 +23,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=POCO/vermeer_global/vermeer:15/AQ3A.240912.001/OS2.0.207.0.VNKMIXM:user/release-keys
+    BuildFingerprint=POCO/vermeer_global/vermeer:16/BP2A.250605.031.A3/OS3.0.6.0.WNKMIXM:user/release-keys
